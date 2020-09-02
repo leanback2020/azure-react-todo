c4eca6f119c845a6efff4663f7b567a65733879f
leanback2020 <post@leanback.no>
2020-09-02 16:59:36 +0200

    Fixed Connection String

--- src/api/GetTodos/index.js
+++ src/api/GetTodos/index.js
@@ -1,18 +1,13 @@
-//const createMongoClient = require("../../shared/mongo")
-
 const { MongoClient } = require("mongodb")
-
 const config = {
-  url: process.env.MONGODB, // "mongodb+srv://mainUser:Mongo@2020@cluster0-z1att.mongodb.net/ReactTodo?retryWrites=true&w=majority", //process.env.CONNECTIONSTRING,
+  url: process.env.MONGODB,
   dbName: "ReactTodo",
 }
 
 async function createConnection() {
-  console.log("Create Connection: " + config.url)
   const connection = await MongoClient.connect(config.url, {
     useNewUrlParser: true,
   })
-  console.log("Create DB Object")
   const db = connection.db(config.dbName)
   return {
     connection,
@@ -21,9 +16,7 @@ async function createConnection() {
 }
 
 module.exports = async (context) => {
-  console.log("Get Todo List")
   const { db, connection } = await createConnection()
-
   const Todos = db.collection("todo")
   const res = await Todos.find({})
   const body = await res.toArray()
