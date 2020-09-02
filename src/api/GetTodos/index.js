aaafe503bad3137029284e9f885b72e3c3a51dde
leanback2020 <post@leanback.no>
2020-09-02 11:28:18 +0200

    Fixed GetTodo Function, unable to load module

--- src/api/GetTodos/index.js
+++ src/api/GetTodos/index.js
@@ -1,6 +1,28 @@
-const createMongoClient = require("../../shared/mongo")
+//const createMongoClient = require("../../shared/mongo")
+
+const { MongoClient } = require("mongodb")
+
+const config = {
+  url: process.env.CONNECTIONSTRING,
+  dbName: "ReactTodo",
+}
+
+async function createConnection() {
+  console.log("Create Connection")
+  const connection = await MongoClient.connect(config.url, {
+    useNewUrlParser: true,
+  })
+  console.log("Create DB Object")
+  const db = connection.db(config.dbName)
+  return {
+    connection,
+    db,
+  }
+}
+
 module.exports = async (context) => {
-  const { db, connection } = await createMongoClient()
+  console.log("Get Todo List")
+  const { db, connection } = await createConnection()
 
   const Todos = db.collection("todo")
   const res = await Todos.find({})
