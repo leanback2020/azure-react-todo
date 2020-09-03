addc336ba4b2d125e3461c795fdb37e1c3a4a15e
leanback2020 <post@leanback.no>
2020-09-03 17:01:27 +0200

    Added progress button

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -26,12 +26,49 @@ export class TodoItem extends Component {
     }
   }
 
+  getStyle2 = () => {
+    if (this.props.todo.completed) {
+      return {
+        background: "#f4f4f4",
+      }
+    } else {
+      return {
+        background: "#f40000",
+      }
+    }
+  }
+
+  state = {
+    text: "Not Started",
+  }
+
+  changeText = (e, text) => {
+    e.preventDefault()
+    if (this.props.todo.completed) {
+      e.target.style.background = "#BFB"
+      text = "Finished"
+    } else {
+      e.target.style.background = "#0F0"
+      e.target.style.border = "none"
+      e.target.style.borderRadius = "20%"
+      e.target.style.outline = "none"
+    }
+    this.setState({ text })
+  }
+
   render() {
+    const { text } = this.state
     const { _id, title } = this.props.todo
     return (
       <div style={this.getStyleShort()}>
         <p>
-          <input type="checkbox" checked={this.props.todo.completed} onChange={this.props.checkComplete.bind(this, _id)} /> {title}
+          <input type="checkbox" checked={this.props.todo.completed} onChange={this.props.checkComplete.bind(this, _id)} /> {title} {"  "}
+          {!this.props.todo.completed && (
+            <button onClick={(e) => this.changeText(e, "InProgress")} style={btnStyleProgress}>
+              {text}
+            </button>
+          )}
+          {this.props.todo.completed && <button style={btnStyleProgress}>Finished</button>}
           <button onClick={this.props.delTodo.bind(this, _id)} style={btnStyle}>
             X
           </button>
@@ -46,6 +83,15 @@ TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
   checkComplete: PropTypes.func.isRequired,
   delTodo: PropTypes.func.isRequired,
+}
+
+const btnStyleProgress = {
+  background: "#ff9900",
+  color: "#fff",
+  border: "none",
+  padding: "5px 9px",
+  borderRadius: "20%",
+  cursor: "pointer",
 }
 
 const btnStyle = {
